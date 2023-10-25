ccb3ea16011f4accaee111c8c38c47d7c63767a9
BinhPham0212 <binhpd0212@gmail.com>
2023-10-26 01:56:40 +0700

    update parallel logintest
    update parallel logintest

--- src/test/java/BinhAT/testcases/LoginTest.java
+++ src/test/java/BinhAT/testcases/LoginTest.java
@@ -19,37 +19,43 @@ import java.util.Hashtable;
 public class LoginTest extends BaseTest {
 
     //Initialize objects
-    LoginPage loginPage = new LoginPage();
-    ExcelHelper excel = new ExcelHelper();
-
-    @Test(priority = 1)
-    public void TC_loginSuccessWithCustomerAccount() {
-        excel.setExcelFile(PropertiesHelper.getValue("EXCEL_CMS_LOGIN"), "Login");
-        loginPage.loginSuccessWithCustomerAccount(excel.getCellData("EMAIL", 2), excel.getCellData("PASSWORD", 2));
-    }
-
-    @Test(priority = 2)
-    public void TC_loginFailWithEmailNull() {
-        excel.setExcelFile(PropertiesHelper.getValue("EXCEL_CMS_LOGIN"), "Login");
-        loginPage.loginFailWithEmailNull(excel.getCellData("PASSWORD", 2));
-    }
-
-    @Test(priority = 3)
+    @Test
     public void TC_loginFailWithInvalidEmail() {
+        LoginPage loginPage = new LoginPage();
+        ExcelHelper excel = new ExcelHelper();
         excel.setExcelFile(PropertiesHelper.getValue("EXCEL_CMS_LOGIN"), "Login");
         loginPage.loginFailWithInvalidEmail(excel.getCellData("EMAIL",3), excel.getCellData("PASSWORD", 3));
     }
 
-    @Test(priority = 4)
-    public void TC_loginFailWithInvalidPassword() {
-        excel.setExcelFile(PropertiesHelper.getValue("EXCEL_CMS_LOGIN"), "Login");
-        loginPage.loginFailWithInvalidPassword(excel.getCellData("EMAIL",4), excel.getCellData("PASSWORD", 4));
+    @Test
+    public void TC_loginSuccessWithCustomerAccount() {
+        LoginPage loginPage = new LoginPage();
+        ExcelHelper excel1 = new ExcelHelper();
+        loginPage.loginSuccessWithCustomerAccount(excel1.getCellData("EMAIL", 2), excel1.getCellData("PASSWORD", 2));
     }
 
-    @Test(priority = 5)
+    @Test
+    public void TC_loginFailWithEmailNull() {
+        LoginPage loginPage = new LoginPage();
+        ExcelHelper excel2 = new ExcelHelper();
+        excel2.setExcelFile(PropertiesHelper.getValue("EXCEL_CMS_LOGIN"), "Login");
+        loginPage.loginFailWithEmailNull(excel2.getCellData("PASSWORD", 2));
+    }
+
+    @Test
+    public void TC_loginFailWithInvalidPassword() {
+        LoginPage loginPage = new LoginPage();
+        ExcelHelper excel3 = new ExcelHelper();
+        excel3.setExcelFile(PropertiesHelper.getValue("EXCEL_CMS_LOGIN"), "Login");
+        loginPage.loginFailWithInvalidPassword(excel3.getCellData("EMAIL",4), excel3.getCellData("PASSWORD", 4));
+    }
+
+    @Test
     public void TC_loginFailWithPasswordNull() {
-        excel.setExcelFile(PropertiesHelper.getValue("EXCEL_CMS_LOGIN"), "Login");
-        loginPage.loginFailWithPasswordNull(excel.getCellData("EMAIL",4));
+        LoginPage loginPage = new LoginPage();
+        ExcelHelper excel4 = new ExcelHelper();
+        excel4.setExcelFile(PropertiesHelper.getValue("EXCEL_CMS_LOGIN"), "Login");
+        loginPage.loginFailWithPasswordNull(excel4.getCellData("EMAIL",4));
     }
 
 }
